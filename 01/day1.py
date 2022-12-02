001f96c75357917ee3e7e3cb122c385bf2d35efb
Ko-nnex <koreosk@gmail.com>
2022-12-02 23:09:02 +0100

    Dumb day 1 solution oneliner :D

--- 01/day1.py
+++ 01/day1.py
@@ -1,7 +1,7 @@
-def part1(lines):
+def part1(data):
     maxelf = 0
     currval = 0
-    for v in lines:
+    for v in data:
         if v == '':
             maxelf = max(currval, maxelf)
             currval = 0
@@ -10,10 +10,10 @@ def part1(lines):
     print(maxelf)
 
 
-def part2(lines):
+def part2(data):
     top = [0, 0, 0]
     currval = 0
-    for v in lines:
+    for v in data:
         if v == '':
             if any(currval > elf for elf in top):
                 top[top.index(min(top))] = currval
@@ -24,7 +24,12 @@ def part2(lines):
     print(sum(top))
 
 
+def better(data):
+    print(sorted([sum([int(number) for number in item.split()]) for item in " ".join(data).split('  ')])[-3:])
+
+
 with open('day1_input.txt', 'r') as f:
     lines = [line.strip() for line in f.readlines()]
-    part1(lines)
-    part2(lines)
+    # part1(lines)
+    # part2(lines)
+    better(lines)

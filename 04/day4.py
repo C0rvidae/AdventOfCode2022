803dc8970f6deb20213d870859b3652b94952df4
YL <-@>
2022-12-08 16:25:34 +0100

    Day 4 part 2

--- 04/day4.py
+++ 04/day4.py
@@ -1,20 +1,19 @@
-def part1(data: list):
-    ctr = 0
-    for item in data:
-        curr = [[int(_) for _ in i.split('-')] for i in item]
-        if (curr[0][0] <= curr[1][0] and curr[0][1] >= curr[1][1]) or \
-                (curr[1][0] <= curr[0][0] and curr[1][1] >= curr[0][1]):
+def part1(data: list, ctr: int):
+    for _ in data:
+        if (_[0][0] <= _[1][0] and _[0][1] >= _[1][1]) or (_[1][0] <= _[0][0] and _[1][1] >= _[0][1]):
             ctr += 1
     print(ctr)
 
 
-def part2(data: list):
-    for item in data:
-        curr = [[int(_) for _ in i.split('-')] for i in item]
+def part2(data: list, ctr: int):
+    for _ in data:
+        if any(a == b for a in range(_[0][0], _[0][1] + 1) for b in range(_[1][0], _[1][1] + 1)):
+            ctr += 1
+    print(ctr)
 
 
 with open('day4_input.txt', 'r') as f:
-    items = [line.strip() for line in f.readlines()]
-    data = [item.split(',') for item in items]
-    # part1(data)
-    part2(data)
+    items = [_.strip() for _ in f.readlines()]
+    data = [[[int(_) for _ in _.split('-')] for _ in _.split(',')] for _ in items]
+    part1(data, 0)
+    part2(data, 0)

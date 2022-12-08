a4bdbe6cd72136bcbf329166abb0815b687adc8b
YL <-@>
2022-12-08 17:17:43 +0100

    Day 4 full golf kekw

--- 04/day4.py
+++ 04/day4.py
@@ -1,16 +1,15 @@
-def part1(data: list, ctr: int):
-    for _ in data:
-        if (_[0][0] <= _[1][0] and _[0][1] >= _[1][1]) or (_[1][0] <= _[0][0] and _[1][1] >= _[0][1]):
-            ctr += 1
-    print(ctr)
+def part1(data: list):
+    print(sum([1 for _ in data
+               if (_[0][0] <= _[1][0] and _[0][1] >= _[1][1]) or (_[1][0] <= _[0][0] and _[1][1] >= _[0][1])]))
 
 
 def part2(data: list):
-    print(sum([1 for _ in data if any(a == b for a in range(_[0][0], _[0][1] + 1) for b in range(_[1][0], _[1][1] + 1))]))
+    print(sum([1 for _ in data
+               if any(a == b for a in range(_[0][0], _[0][1] + 1) for b in range(_[1][0], _[1][1] + 1))]))
 
 
 with open('day4_input.txt', 'r') as f:
     items = [_.strip() for _ in f.readlines()]
     data = [[[int(_) for _ in _.split('-')] for _ in _.split(',')] for _ in items]
-    part1(data, 0)
+    part1(data)
     part2(data)
